c38f60c31a8199a3e63b5fb644115f9446e21a75
remorses <beats.by.morse@gmail.com>
2020-07-13 15:22:43 +0200

    changed logo

--- template/components/StoriesIndex.tsx
+++ template/components/StoriesIndex.tsx
@@ -92,14 +92,82 @@ export const Logo = ({ ...rest }) => {
     return (
         <Stack
             direction='row'
-            align='center'
+            align='baseline'
             fontSize='24px'
-            fontWeight='600'
+            fontWeight='500'
             spacing='3'
             {...rest}
         >
-            <Box as={LogoPart} size='1.1em' />
-            <Box fontSize='32px'>Vitrine</Box>
+            <Box as={Beaker} size='1.5em' />
+            <Box fontSize='42px'>Vitro</Box>
         </Stack>
+    )
+}
+
+const Beaker = ({ ...rest }) => {
+    return (
+        <svg viewBox='0 0 73 74' version='1.1' {...rest}>
+            <title>BeakerLogo</title>
+            <desc>Created with Sketch.</desc>
+            <g
+                id='Page-1'
+                stroke='none'
+                stroke-width='1'
+                fill='none'
+                fill-rule='evenodd'
+            >
+                <g
+                    id='Desktop-HD'
+                    transform='translate(-1253.000000, -683.000000)'
+                >
+                    <g
+                        id='BeakerLogo'
+                        transform='translate(1256.000000, 685.000000)'
+                    >
+                        <path
+                            d='M7.10699457,2.96165066 L7.10699457,63.5079402 C7.10699457,66.269364 9.34557082,68.5079402 12.1069946,68.5079402 L54.7969833,68.5079402 C57.5584071,68.5079402 59.7969833,66.269364 59.7969833,63.5079402 L59.7969833,2.96165066 L59.7969833,2.96165066'
+                            id='Path-2'
+                            stroke='currentColor'
+                            stroke-width='5'
+                        ></path>
+                        <path
+                            d='M58.5,31.5 L58.5,67.5 L8.5,67.5 L8.5,31.5 L58.5,31.5 Z M39,45.5 C35.9624339,45.5 33.5,47.9624339 33.5,51 C33.5,54.0375661 35.9624339,56.5 39,56.5 C42.0375661,56.5 44.5,54.0375661 44.5,51 C44.5,47.9624339 42.0375661,45.5 39,45.5 Z M25,47.5 C23.0670034,47.5 21.5,49.0670034 21.5,51 C21.5,52.9329966 23.0670034,54.5 25,54.5 C26.9329966,54.5 28.5,52.9329966 28.5,51 C28.5,49.0670034 26.9329966,47.5 25,47.5 Z'
+                            id='Combined-Shape'
+                            fill='currentColor'
+                        ></path>
+                        <line
+                            x1='67'
+                            y1='1'
+                            x2='0'
+                            y2='1'
+                            id='Line-Copy-2'
+                            stroke='currentColor'
+                            stroke-width='5'
+                            stroke-linecap='round'
+                        ></line>
+                        <line
+                            x1='28'
+                            y1='11'
+                            x2='19'
+                            y2='11'
+                            id='Line'
+                            stroke='currentColor'
+                            stroke-width='5'
+                            stroke-linecap='round'
+                        ></line>
+                        <line
+                            x1='28'
+                            y1='21'
+                            x2='19'
+                            y2='21'
+                            id='Line-Copy'
+                            stroke='currentColor'
+                            stroke-width='5'
+                            stroke-linecap='round'
+                        ></line>
+                    </g>
+                </g>
+            </g>
+        </svg>
     )
 }
