36b5baa10d7ea64e0a086425734609b677b1a0c7
remorses <beats.by.morse@gmail.com>
2020-07-14 19:14:48 +0200

    npm: logo has jsx good props

--- packages/ui/src/stories_nav.tsx
+++ packages/ui/src/stories_nav.tsx
@@ -102,7 +102,7 @@ export const Logo = ({ ...rest }) => {
             spacing='3'
             {...rest}
         >
-            <Box as={Beaker} size='1.6em' />
+            <Box as={Beaker} size='1.4em' />
             <Box letterSpacing='0.06em' fontSize='1.6em'>
                 Vitro
             </Box>
@@ -118,9 +118,9 @@ const Beaker = ({ ...rest }) => {
             <g
                 id='Page-1'
                 stroke='none'
-                stroke-width='1'
+                strokeWidth={1}
                 fill='none'
-                fill-rule='evenodd'
+                fillRule='evenodd'
             >
                 <g
                     id='Desktop-HD'
@@ -134,43 +134,43 @@ const Beaker = ({ ...rest }) => {
                             d='M7.10699457,2.96165066 L7.10699457,63.5079402 C7.10699457,66.269364 9.34557082,68.5079402 12.1069946,68.5079402 L54.7969833,68.5079402 C57.5584071,68.5079402 59.7969833,66.269364 59.7969833,63.5079402 L59.7969833,2.96165066 L59.7969833,2.96165066'
                             id='Path-2'
                             stroke='currentColor'
-                            stroke-width='5'
-                        ></path>
+                            strokeWidth={5}
+                        />
                         <path
                             d='M58.5,31.5 L58.5,67.5 L8.5,67.5 L8.5,31.5 L58.5,31.5 Z M39,45.5 C35.9624339,45.5 33.5,47.9624339 33.5,51 C33.5,54.0375661 35.9624339,56.5 39,56.5 C42.0375661,56.5 44.5,54.0375661 44.5,51 C44.5,47.9624339 42.0375661,45.5 39,45.5 Z M25,47.5 C23.0670034,47.5 21.5,49.0670034 21.5,51 C21.5,52.9329966 23.0670034,54.5 25,54.5 C26.9329966,54.5 28.5,52.9329966 28.5,51 C28.5,49.0670034 26.9329966,47.5 25,47.5 Z'
                             id='Combined-Shape'
                             fill='currentColor'
-                        ></path>
+                        />
                         <line
-                            x1='67'
-                            y1='1'
-                            x2='0'
-                            y2='1'
+                            x1={67}
+                            y1={1}
+                            x2={0}
+                            y2={1}
                             id='Line-Copy-2'
                             stroke='currentColor'
-                            stroke-width='5'
-                            stroke-linecap='round'
-                        ></line>
+                            strokeWidth={5}
+                            strokeLinecap='round'
+                        />
                         <line
-                            x1='28'
-                            y1='11'
-                            x2='19'
-                            y2='11'
+                            x1={28}
+                            y1={11}
+                            x2={19}
+                            y2={11}
                             id='Line'
                             stroke='currentColor'
-                            stroke-width='5'
-                            stroke-linecap='round'
-                        ></line>
+                            strokeWidth={5}
+                            strokeLinecap='round'
+                        />
                         <line
-                            x1='28'
-                            y1='21'
-                            x2='19'
-                            y2='21'
+                            x1={28}
+                            y1={21}
+                            x2={19}
+                            y2={21}
                             id='Line-Copy'
                             stroke='currentColor'
-                            stroke-width='5'
-                            stroke-linecap='round'
-                        ></line>
+                            strokeWidth={5}
+                            strokeLinecap='round'
+                        />
                     </g>
                 </g>
             </g>
