0d282937ee3a8004b1f92d276bf471f0e2f69061
remorses <beats.by.morse@gmail.com>
2020-07-14 18:50:47 +0200

    moved home in separate file

--- packages/template/pages/index.tsx
+++ packages/template/pages/index.tsx
@@ -1,23 +1,3 @@
-import { Box, Stack } from '@chakra-ui/core'
+import {Home} from '@vitro/ui/src'
 
-export default function Page(props) {
-    return (
-        <Stack px='10' align='center'>
-            <Stack spacing='10' maxW='500px' align='center' justify='center'>
-                <Box lineHeight='1.5em' fontSize='2em' fontWeight='medium'>
-                    Storyboards helps you build react components in isolation
-                </Box>
-                <Box
-                    lineHeight='1.5em'
-                    fontSize='1.4em'
-                    opacity={0.6}
-                    fontWeight='medium'
-                >
-                    Navigate to a story page to see the rendered exported
-                    components
-                </Box>
-                {/* <Image w='300px' src={path.join(BASE_PATH, 'share-reuse.png')} /> */}
-            </Stack>
-        </Stack>
-    )
-}
+export default Home
---
+++ packages/ui/src/home.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box, Stack } from '@chakra-ui/core'
+
+export function Home(props) {
+    return (
+        <Stack px='10' align='center'>
+            <Stack spacing='10' maxW='500px' align='center' justify='center'>
+                <Box lineHeight='1.5em' fontSize='2em' fontWeight='medium'>
+                    Storyboards helps you build react components in isolation
+                </Box>
+                <Box
+                    lineHeight='1.5em'
+                    fontSize='1.4em'
+                    opacity={0.6}
+                    fontWeight='medium'
+                >
+                    Navigate to a story page to see the rendered exported
+                    components
+                </Box>
+                {/* <Image w='300px' src={path.join(BASE_PATH, 'share-reuse.png')} /> */}
+            </Stack>
+        </Stack>
+    )
+}
