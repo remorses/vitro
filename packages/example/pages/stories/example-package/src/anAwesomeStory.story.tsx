6e3bb614c73266afd535f890ea09b4900ef6842a
remorses <beats.by.morse@gmail.com>
2020-07-17 14:04:22 +0200

    fixed building of example

--- packages/example/pages/stories/example-package/src/anAwesomeStory.story.tsx
+++ packages/example/pages/stories/example-package/src/anAwesomeStory.story.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as exported from '@/../example-package/src/anAwesomeStory.story'
 import { default as GlobalWrapper } from '@/../example-package/src/Wrapper'
 import storiesMap from '@/storiesMap'
-import { StoryPage } from '@vitro/ui/src'
+import { StoryPage } from '@vitro/ui'
 
 const absolutePath = '/Users/morse/Documents/GitHub/react-comics/packages/example-package/src/anAwesomeStory.story.tsx'
 
--- packages/example/pages/stories/example-package/src/stillAnotherStory.story.tsx
+++ packages/example/pages/stories/example-package/src/stillAnotherStory.story.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as exported from '@/../example-package/src/stillAnotherStory.story'
 import { default as GlobalWrapper } from '@/../example-package/src/Wrapper'
 import storiesMap from '@/storiesMap'
-import { StoryPage } from '@vitro/ui/src'
+import { StoryPage } from '@vitro/ui'
 
 const absolutePath = '/Users/morse/Documents/GitHub/react-comics/packages/example-package/src/stillAnotherStory.story.tsx'
 
