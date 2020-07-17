21e91172a5cab2667db379faedcffc2506525ef5
remorses <beats.by.morse@gmail.com>
2020-07-17 14:31:44 +0200

    smaller nprogress

--- packages/ui/src/css.ts
+++ packages/ui/src/css.ts
@@ -15,7 +15,7 @@ const nprogressStyles = css`
         left: 0;
 
         width: 100%;
-        height: 5px;
+        height: 4px;
     }
 
     /* Fancy blur effect */
@@ -25,7 +25,7 @@ const nprogressStyles = css`
         right: 0px;
         width: 100px;
         height: 100%;
-        box-shadow: 0 0 5px #29d, 0 0 3px #29d;
+        box-shadow: 0 0 3px #29d, 0 0 3px #29d;
         opacity: 1;
 
         -webkit-transform: rotate(3deg) translate(0px, -4px);
@@ -40,20 +40,6 @@ const nprogressStyles = css`
         z-index: 1031;
         top: 15px;
         right: 25px;
-    }
-
-    #nprogress .spinner-icon {
-        width: 18px;
-        height: 18px;
-        box-sizing: border-box;
-
-        border: solid 3px transparent;
-        border-top-color: #29d;
-        border-left-color: #29d;
-        border-radius: 50%;
-
-        -webkit-animation: nprogress-spinner 400ms linear infinite;
-        animation: nprogress-spinner 400ms linear infinite;
     }
 
     .nprogress-custom-parent {
