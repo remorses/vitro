55cf4b05c5c2df80e7dfe96e358b63df9737cd34
remorses <beats.by.morse@gmail.com>
2020-07-30 20:58:43 +0200

    added the config docs

--- packages/cli/src/contsants.ts
+++ packages/cli/src/contsants.ts
@@ -10,9 +10,16 @@ export const TEMPLATE_PATH = TESTING
     ? path.resolve(__dirname, '../../template')
     : path.resolve(__dirname, '../template')
 
+// TODO add more options commented out and the docs link for the configuration object
 export const DEFAULT_CONFIG = `
+// read more at https://vitro.now.sh/docs/config
 module.exports = {
-    experiments: ['./**',],
+    experiments: ['./**/*.vitro.jsx'], // globs to search for experiment files
+    globalCSS: [], // path to css files to use to for all experiments
+    ignore: [], // add directories to ignore when searching for experiment files
+    transpileModules: [], // list of packages to transpile
+    // wrapper: './wrapper.tsx' // file that has a wrapper component as default export 
+    // basePath: '/vitro-app', // modifies the deployed base path
 }
 `
 
