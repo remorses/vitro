7954a2c4b44e3249adf85c83272235c5ba813d7d
remorses <beats.by.morse@gmail.com>
2020-07-30 21:43:55 +0200

    nn

--- examples/renderer/pages/experiments/example-package/src/stillAnotherExperiment.vitro.tsx
+++ examples/renderer/pages/experiments/example-package/src/stillAnotherExperiment.vitro.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import * as exported from '@/../example-package/src/stillAnotherExperiment.vitro'
-import { default as GlobalWrapper } from '@/../@vitro/ui/dist/components/DefaultWrapper'
+import { default as GlobalWrapper } from '@/../@vitro/ui/src/components/DefaultWrapper'
 import experimentsMap from '@/experimentsMap'
-import { ExperimentPage } from '@vitro/ui'
+import { ExperimentPage } from '@vitro/ui/src'
 
 const absolutePath = '/Users/morse/Documents/GitHub/react-comics/examples/example-package/src/stillAnotherExperiment.vitro.tsx'
 
