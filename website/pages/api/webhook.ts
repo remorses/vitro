91ed9bf299d5361e2b763a12f9849f3ae16c5738
remorses <beats.by.morse@gmail.com>
2020-08-17 12:52:13 +0200

    made the webohok page

---
+++ website/pages/api/webhook.ts
@@ -0,0 +1,22 @@
+import { NextApiHandler } from 'next/types'
+import { Webhooks } from '@octokit/webhooks'
+
+const webhooks = new Webhooks({
+    secret: 'mysecret',
+})
+
+webhooks.on('push', (ev) => {
+    console.log(ev.name, 'event received')
+    console.log(JSON.stringify(ev.payload, null, 4))
+    const { ref, commits, pusher, installation, repository } = ev.payload
+    repository.html_url
+    pusher.name
+    pusher.email
+    // TODO check if a vitro.config.js exists, if yes then create a new vercel deployment, after deploy, after deploy run the screenshotter and update database with screenshots locations ...
+})
+
+const handler: NextApiHandler = async (req, res) => {
+    await webhooks.middleware(req, res, (err) => {
+        console.error(err)
+    })
+}
