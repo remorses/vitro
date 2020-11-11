5509c863ff5743e1d5575e132a20a76a2195d938
remorses <beats.by.morse@gmail.com>
2020-11-11 17:49:26 +0100

    changed template

--- packages/template/babel.config.js
+++ packages/template/babel.config.js
@@ -1,4 +1,9 @@
+const locationInjectPlugin = require('@vitro/babel-plugin-inject-location')
+
 module.exports = {
     presets: ['next/babel'],
-    plugins: ['babel-plugin-macros'],
+    plugins: [
+        'babel-plugin-macros',
+        locationInjectPlugin.default, // inject the filename and line number
+    ],
 }
