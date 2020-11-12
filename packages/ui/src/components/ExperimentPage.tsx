8d024bc9502c9d689319d3891bd1ad6dabde63ae
remorses <beats.by.morse@gmail.com>
2020-11-12 14:30:04 +0100

    split logic to make it reusable in other projects

--- packages/ui/src/components/ExperimentPage.tsx
+++ packages/ui/src/components/ExperimentPage.tsx
@@ -21,9 +21,12 @@ import React, {
     useMemo,
     useRef,
     useState,
+    createContext,
+    ReactNode,
+    HTMLAttributes,
 } from 'react'
 import assign from 'lodash/assign'
-import { FaBug, FaLink } from 'react-icons/fa'
+import { FaBug, FaLink, FaUnderline } from 'react-icons/fa'
 import { FiHash, FiZap } from 'react-icons/fi'
 import { MdFullscreen, MdFullscreenExit } from 'react-icons/md'
 import { isValidElementType } from 'react-is'
@@ -33,6 +36,7 @@ import { MobileNav } from './MobileNav'
 import { formatPathToTitle, TOP_TITLE_H, usePromise, debug } from '../support'
 import styled from '@emotion/styled'
 import { MdxStyler } from './MarkdownStyler'
+import ReactDOM from 'react-dom'
 
 jsx
 
@@ -77,56 +81,9 @@ export function ExperimentPage({
         }
     }, [GlobalWrapper])
 
-    const [clickToSeeSourceOption, setClickToSeeSource] = useState('disabled')
-    const clickToSourceEnabled = clickToSeeSourceOption !== 'disabled'
-    useEffect(() => {
-        if (clickToSourceEnabled) {
-            const elements = Array.from(
-                document.querySelectorAll(
-                    '.vitro-block *[data-vitro-filename]',
-                ),
-            )
-
-            elements.forEach((elem) => {
-                elem.addEventListener('click', onClickToSource)
-            })
-        } else {
-            const elements = Array.from(
-                document.querySelectorAll('*[data-vitro-filename]'),
-            )
-            elements.forEach((elem) => {
-                elem.removeEventListener('click', onClickToSource)
-            })
-        }
-    }, [clickToSourceEnabled])
-
-    const onClickToSource = useCallback(
-        function onClickToSource(e) {
-            // e.preventDefault()
-            e.stopPropagation()
-            setClickToSeeSource('disabled')
-            const target = e.currentTarget
-            const filename = target.getAttribute('data-vitro-filename')
-            // TODO filename is a path realtive to the root of the repository, this path can be used on vscode or on github using different path.join
-            const lineNumber = target.getAttribute('data-vitro-line')
-            if (!filename) {
-                console.warn(
-                    `no filename found among ${[target.attributes]
-                        .map((x) => `${x.name}='${x.value}'`)
-                        .join(', ')}`,
-                )
-                return
-            }
-            if (!lineNumber) {
-                console.warn(
-                    `no line number found for ${target} among ${target.attributes}`,
-                )
-                return
-            }
-            window.location.href = `vscode://file${filename}:${lineNumber}`
-        },
-        [setClickToSeeSource],
-    )
+    const [clickToSeeSourceProvider, setClickToSeeSource] = useState<
+        ClickToSourceState['provider']
+    >('')
 
     const vscodeUrl = `vscode://file${sourceExperimentPath}`
     // exported.then(z => console.log(Object.keys(z)))
@@ -212,26 +169,25 @@ export function ExperimentPage({
 
                 <Select
                     onChange={(e) => {
-                        const value = e.target.value
+                        const value = e.target.value as any
                         setClickToSeeSource(value)
                     }}
-                    value={clickToSeeSourceOption}
+                    fontWeight={500}
+                    value={clickToSeeSourceProvider}
                     opacity={0.8}
                     variant='filled'
                     bg={bg}
                     // placeholder='click to see source'
                     w='auto'
                 >
-                    <option
-                        value='disabled'
-                        children='click to source disabled'
-                    />
+                    <option value='' children='click to source disabled' />
                     <option
                         value='vscode'
                         children='click to source on Vscode'
                     />
                 </Select>
             </Stack>
+
             <Stack
                 flexShrink={0}
                 // direction='row'
@@ -270,41 +226,188 @@ export function ExperimentPage({
                                     key={id}
                                     id={id}
                                 >
-                                    <Stack
-                                        // flex='1'
-                                        className={`${
-                                            clickToSourceEnabled
-                                                ? 'vitro-block'
-                                                : ''
-                                        }`}
-                                        w='100%'
-                                        h='100%'
-                                        minH='100%'
-                                        spacing='0'
-                                        align='center'
-                                        justify='center'
-                                        as={cssDebugEnabled ? DebugCSS : 'div'}
+                                    <ClickToSourceProviderStateless
+                                        value={{
+                                            provider: clickToSeeSourceProvider,
+                                        }}
+                                        onChange={(state) => {
+                                            setClickToSeeSource(state.provider)
+                                        }}
                                     >
-                                        <ValidGlobalWrapper
-                                            key={colorMode} // TODO remounting on color mode change or the providers get fucked up
-                                            dark={colorMode == 'dark'}
+                                        <Stack
+                                            // flex='1'
+                                            w='100%'
+                                            h='100%'
+                                            minH='100%'
+                                            spacing='0'
+                                            align='center'
+                                            justify='center'
+                                            as={
+                                                cssDebugEnabled
+                                                    ? DebugCSS
+                                                    : 'div'
+                                            }
                                         >
-                                            <ExperimentWrapper
-                                                key={colorMode}
+                                            <ValidGlobalWrapper
+                                                key={colorMode} // TODO remounting on color mode change or the providers get fucked up
                                                 dark={colorMode == 'dark'}
                                             >
-                                                <Component
+                                                <ExperimentWrapper
                                                     key={colorMode}
                                                     dark={colorMode == 'dark'}
-                                                />
-                                            </ExperimentWrapper>
-                                        </ValidGlobalWrapper>
-                                    </Stack>
+                                                >
+                                                    <Component
+                                                        key={colorMode}
+                                                        dark={
+                                                            colorMode == 'dark'
+                                                        }
+                                                    />
+                                                </ExperimentWrapper>
+                                            </ValidGlobalWrapper>
+                                        </Stack>
+                                    </ClickToSourceProviderStateless>
                                 </StoryBlock>
                             )
                         })}
             </Stack>
         </Stack>
+    )
+}
+
+type ClickToSourceState = {
+    provider: '' | 'vscode'
+}
+
+const ClickToSourceContext = createContext<ClickToSourceState>({ provider: '' })
+
+function ClickToSourceProvider({ children = null }) {
+    const [state, setState] = useState<ClickToSourceState>({ provider: '' })
+    const [elem] = useState(document.createElement('div'))
+
+    useEffect(() => {
+        document.body.appendChild(elem)
+    }, [])
+
+    const selectElement = useMemo(
+        () => (
+            <Box p='10' position='fixed' top='0' right='0'>
+                <Select
+                    onChange={(e) => {
+                        const value = e.target.value as any
+                        setState((state) => ({ ...state, provider: value }))
+                    }}
+                    fontWeight={500}
+                    value={state.provider}
+                    opacity={0.8}
+                    variant='filled'
+                    // placeholder='click to see source'
+                    w='auto'
+                >
+                    <option value='' children='click to source disabled' />
+                    <option
+                        value='vscode'
+                        children='click to source on Vscode'
+                    />
+                </Select>
+            </Box>
+        ),
+        [setState, state],
+    )
+
+    return (
+        <ClickToSourceProviderStateless
+            value={state}
+            onChange={(x) => setState(x)}
+        >
+            {ReactDOM.createPortal(selectElement, elem)}
+            {children}
+        </ClickToSourceProviderStateless>
+    )
+}
+
+function ClickToSourceProviderStateless({
+    value = { provider: '' },
+    onChange,
+    className = '',
+    children = null,
+    ...rest
+}: HTMLAttributes<HTMLDivElement> & {
+    value: ClickToSourceState
+    onChange: (x: ClickToSourceState) => any
+}) {
+    // const [state, setState] = useState<ClickToSourceState>(value)
+    const enabled = Boolean(value.provider)
+    const ref = useRef()
+    const disable = useCallback(
+        function () {
+            onChange({ ...value, provider: '' })
+        },
+        [onChange],
+    )
+    const onClickToSource = useCallback(function onClickToSource(e) {
+        // e.preventDefault()
+        e.stopPropagation()
+        disable()
+        const target = e.currentTarget
+        const filename = target.getAttribute('data-vitro-filename')
+        // TODO filename is a path realtive to the root of the repository, this path can be used on vscode or on github using different path.join
+        const lineNumber = target.getAttribute('data-vitro-line')
+        if (!filename) {
+            console.warn(
+                `no filename found among ${[target.attributes]
+                    .map((x) => `${x.name}='${x.value}'`)
+                    .join(', ')}`,
+            )
+            return
+        }
+        if (!lineNumber) {
+            console.warn(
+                `no line number found for ${target} among ${target.attributes}`,
+            )
+            return
+        }
+        window.location.href = `vscode://file${filename}:${lineNumber}`
+    }, [])
+    useEffect(() => {
+        const root: Document = ref.current
+        if (!root) {
+            console.error(new Error(`no root ref`))
+        }
+        if (enabled) {
+            const elements = Array.from(
+                root.querySelectorAll('.vitro-block *[data-vitro-filename]'),
+            )
+
+            elements.forEach((elem) => {
+                elem.addEventListener('click', onClickToSource)
+            })
+        } else {
+            const elements = Array.from(
+                root.querySelectorAll('*[data-vitro-filename]'),
+            )
+            elements.forEach((elem) => {
+                elem.removeEventListener('click', onClickToSource)
+            })
+        }
+    }, [enabled])
+    const enabledClassName = enabled ? 'vitro-block' : ''
+    const contextValue = {
+        ...value,
+    }
+    return (
+        <ClickToSourceContext.Provider value={contextValue}>
+            <div
+                className={
+                    className
+                        ? `${className} ${enabledClassName}`
+                        : enabledClassName
+                }
+                ref={ref}
+                {...rest}
+            >
+                {children}
+            </div>
+        </ClickToSourceContext.Provider>
     )
 }
 
