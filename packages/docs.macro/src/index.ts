693b43585a584a94f131a39eb554917c0dfe4aff
remorses <beats.by.morse@gmail.com>
2021-01-25 17:24:45 +0100

    fix ts error for macro

--- packages/docs.macro/src/index.ts
+++ packages/docs.macro/src/index.ts
@@ -1,5 +1,1 @@
-const macro = require('./macro')
-
-module.exports = macro
-
-export declare const docs: any
+export {}
--- website/constants.ts
+++ website/constants.ts
@@ -1,3 +1,3 @@
 export const GITHUB_LINK = 'https://github.com/remorses/vitro'
-export const VITRO_DEMO_URL = 'https://vitro-demo.now.sh'
+export const VITRO_DEMO_URL = 'https://vitro-demos.now.sh'
 
